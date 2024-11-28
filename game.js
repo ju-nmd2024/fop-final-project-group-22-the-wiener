c60a0742f420d2a3eccfd58ac454f1303f2f05d4
angesalazars <angelicasalazar2828@gmail.com>
2024-11-28 15:19:43 +0100

    uploaded background of level 1/ angelica

--- game.js
+++ game.js
@@ -1,7 +1,19 @@
+// variable for guetto image 
+let img;
+
+function preload() {
+  img = loadImage("ghetto.png");
+}
+
+
 function setup() {
   createCanvas(800, 600);
 }
 
 function draw() {
-  background(255, 140, 0);
+  background(0, 0, 0);
+  //image guetto
+  imageMode (CENTER);
+  image(img, width/2, height/2, width, height);
 }
+
